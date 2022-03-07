95516c402be3fc2d4e1437cc1ce1e3032c844282
ptrrk1337 <mymyry1999@gmail.com>
2022-03-07 15:39:31 +0100

    GAMETRADER-41 Initialized support service

---
+++ src/main/java/com/gametrader/gametradersupportservice/GametraderSupportServiceApplication.java
@@ -0,0 +1,13 @@
+package com.gametrader.gametradersupportservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GametraderSupportServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GametraderSupportServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/gametrader/gametradersupportservice/GametraderSupportServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gametrader.gametradersupportservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GametraderSupportServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
